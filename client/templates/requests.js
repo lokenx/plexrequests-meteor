a6be349fc8a02566ad89316f32b4d5721397c3af
Mikhail Delport <mikhail@delport.ca>
2015-03-16 22:51:48 -0400

    Removed unneeded requests

--- client/templates/requests.js
+++
@@ -1,6 +0,0 @@
-Template.requests.events({
-    'click #requests': function(event){
-        document.getElementById("reqmov").className = "row col-md-offset-3 col-md-7";
-        return false;
-    }
-});
