72ea29eaf3325e8a2ffcab456f515cb3692aab5a
Mikhail Delport <mikhail@delport.ca>
2015-03-16 22:46:40 -0400

    Added session helpers

---
+++ client/helpers/helpers.js
@@ -0,0 +1,7 @@
+UI.registerHelper('resultsloaded',function(input){
+  return Session.get("resultsloaded");
+});
+
+UI.registerHelper('searchingresults',function(input){
+  return Session.get("searchingresults");
+});
