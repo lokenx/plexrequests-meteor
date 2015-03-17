cc81d9da3888ce4f38ad09181dd7655881c88f33
Mikhail Delport <mikhail@delport.ca>
2015-03-16 22:49:40 -0400

    Created session variables, changed spinner color

--- client/main.js
+++ client/main.js
@@ -1,4 +1,7 @@
 MovieSearch = new Mongo.Collection("moviesearch");
+Session.set('resultsloaded', false);
+Session.set('searchingresults', false);
+Meteor.Spinner.options = {color: "#DD6928"};
 
 $("#showmodal").on("click", function() {
     $('#myModal').modal('show');
