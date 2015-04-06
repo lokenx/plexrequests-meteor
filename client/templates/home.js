8f23528b47f1d4dbc1e822aa483a1768f933150f
Mikhail Delport <mikhail@delport.ca>
2015-04-05 20:20:34 -0400

    Improved filtering of results

--- client/templates/home.js
+++ client/templates/home.js
@@ -2,8 +2,8 @@ Template.home.helpers({
     movie: function () {
         var date = new Date(+new Date - 12096e5);
         return Movies.find({$or: [
-            { $and : [ { downloaded : false } ] },
-            { $and : [ { downloaded : true } , { createdAt: {"$gte": date} } ] }
+            { downloaded : false },
+            { downloaded : true , createdAt: {"$gte": date} }
         ]},
             {sort:{createdAt:-1}});
     },
