7d4e224fb6f2acbf89b966e98e8975d76607481c
Mikhail Delport <mikhail@delport.ca>
2015-04-06 20:22:04 -0400

    Cleaned up main.js file

--- client/main.js
+++ client/main.js
@@ -1,22 +1,6 @@
-MovieSearch = new Mongo.Collection("moviesearch");
-Session.set('resultsloaded', false);
-Session.set('searchingresults', false);
-Session.set('noresults', false);
-Session.set('searcherror', false);
-Session.set('requests', false);
-Session.set('movieadded', false);
-Session.set('movieexists', false);
-Session.set('moviedownloaded', false);
-
-$("#showmodal").on("click", function() {
-    $('#myModal').modal('show');
-    return false;
-});
-
 Router.configure({
   loadingTemplate: 'loading'
 });
-
 
 Router.configure({
   notFoundTemplate: "NotFound"
@@ -30,5 +14,15 @@ Router.route('/couchpotato', function () {
   this.render('couchpotato');
 });
 
+MovieSearch = new Mongo.Collection("moviesearch");
 Meteor.subscribe('movies');
 Meteor.subscribe('settings');
+
+Session.set('resultsloaded', false);
+Session.set('searchingresults', false);
+Session.set('noresults', false);
+Session.set('searcherror', false);
+Session.set('requests', false);
+Session.set('movieadded', false);
+Session.set('movieexists', false);
+Session.set('moviedownloaded', false);
