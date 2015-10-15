c363537c983de38edf465c282ca0463ac4b80fae
Mikhail Delport <mikhail@delport.ca>
2015-10-15 15:45:14 -0400

    Method file renames

--- server/startup/settings.js
+++ server/startup/settings.js
@@ -6,13 +6,10 @@ Meteor.startup(function () {
 	}
 
 	//set TheMovieDB API
-
 	TMDBSearch.api = "95a281fbdbc2d2b7db59680dade828a6";
 	TMDBSearch.language = "en";
 
-
 	//set Couch Potato on start-up
-
 	CouchPotato.url = (Settings.find({}).fetch()[0].couchPotatoSSL) ? "https://" + Settings.find({}).fetch()[0].couchPotatoURL : "http://" + Settings.find({}).fetch()[0].couchPotatoURL;
 	CouchPotato.port = Settings.find({}).fetch()[0].couchPotatoPORT;
 	CouchPotato.api = Settings.find({}).fetch()[0].couchPotatoAPI;
