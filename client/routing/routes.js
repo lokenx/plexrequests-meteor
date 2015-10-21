4aaba3fd2391e1f77184755674a2ada7aeb3a5a9
Mikhail Delport <mikhail@delport.ca>
2015-10-21 11:25:47 -0400

    Added routes/yields

--- client/routing/routes.js
+++ client/routing/routes.js
@@ -1,7 +1,12 @@
-Router.route('/', function () {
-    this.render('home');
+Router.configure({
+  layoutTemplate: 'home'
+});
+
+Router.route('/search', function () {
+  this.render('search');
 });
 
 Router.route('/requests', function () {
 	this.render('requests');
-})
+});
+
