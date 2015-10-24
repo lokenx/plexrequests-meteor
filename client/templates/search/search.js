f1ce00dea9222d6d9f8de44270446b481a4d8608
Mikhail Delport <mikhail@delport.ca>
2015-10-24 18:21:07 -0400

    Request button cleanup

--- client/templates/search/search.js
+++ client/templates/search/search.js
@@ -82,7 +82,8 @@ Template.search.events({
   },
   'click .add-request': function (event) {
     var btn = $(event.target);
-    var tvBtn = $(".tv-request");
+    var tvBtn = $(event.target).parent().parent().children();
+    console.log(tvBtn);
     var requestTitle = this.title;
     var request = this;
     request.user = Session.get("user");
@@ -97,28 +98,28 @@ Template.search.events({
           Bert.alert("Couldn't submit request, please try again!", "danger");
         } else if (result === true) {
           Bert.alert("Successfully requested " + requestTitle + "!", "success");
-          btn.hide();
+          // btn.hide();
         } else if (result === "limit") {
           Bert.alert("You've exceeded your weekly limit!", "warning");
           btn.html('<i class="fa fa-plus"></i> &nbsp; Request');
         } else if (result === 'exists') {
           Bert.alert("Movie has already been requested!", "warning");
-          btn.hide();
+          // btn.hide();
         }
       })
     } else if (this.media_type === "tv") {
-      // tvBtn.html('<i class="fa fa-spinner fa-spin"></i> &nbsp; Requesting...');
+      tvBtn.html('<i class="fa fa-spinner fa-spin"></i> &nbsp; Requesting... ');
       Meteor.call("requestTV", request, function (error, result) {
         if (error || result === false) {
           console.log("Error requesting, check server log");
-          // tvBtn.html('<i class="fa fa-plus"></i> &nbsp; Request');
+          tvBtn.html('<i class="fa fa-plus"></i> &nbsp; Request ');
           Bert.alert("Couldn't submit request, please try again!", "danger");
         } else if (result === true) {
           Bert.alert("Successfully requested " + requestTitle + "!", "success");
           // tvBtn.hide();
         } else if (result === "limit") {
           Bert.alert("You've exceeded your weekly limit!", "warning");
-          // tvBtn.html('<i class="fa fa-plus"></i> &nbsp; Request');
+          tvBtn.html('<i class="fa fa-plus"></i> &nbsp; Request ');
         } else if (result === 'exists') {
           Bert.alert("Movie has already been requested!", "warning");
           // tvBtn.hide();
