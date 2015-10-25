8d3e83e8034be1c903fc352e1fffc18ae37b26b9
Mikhail Delport <mikhail@delport.ca>
2015-10-25 17:16:11 -0400

    Secured and cleaned up subscriptions inside routes, added loading template and progress bar

--- client/routing/routes.js
+++ client/routing/routes.js
@@ -1,5 +1,7 @@
 Router.configure({
-  layoutTemplate: 'layout'
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  progressSpinner : false
 });
 
 Router.route('/', function () {
@@ -10,30 +12,45 @@ Router.route('/', function () {
 	}
 });
 
-Router.route('/search', function () {
-  if (Session.get("auth")) {
-		this.render('search');
-	} else {
-		this.redirect('/');
-	}
+Router.route('/search', {
+  name: 'search',
+  waitOn: function () {
+    return [Meteor.subscribe('movies'),Meteor.subscribe('tv')];
+  },
+  onBeforeAction: function () {
+    if (Session.get("auth")) {
+      this.next();
+    } else {
+      this.redirect('/');
+    }
+  }
 });
 
-Router.route('/requests', function () {
-	if (Session.get("auth")) {
-		this.render('requests');
-	} else if (Meteor.userId()) {
-		this.render('requests');
-	} else {
-		this.redirect('/');
-	}
+Router.route('/requests', {
+  name: 'requests',
+  waitOn: function () {
+    return [Meteor.subscribe('movies'),Meteor.subscribe('tv')];
+  },
+  onBeforeAction: function () {
+    if (!Session.get("auth") && !Meteor.userId()) {
+      this.redirect('/');
+    } else {
+      this.next();
+    }
+  }
 });
 
-Router.route('/admin', function () {
-	if (Meteor.userId()) {
-		this.render('admin', {
-	    data: function () { return Settings.findOne({})}
-	  });	
-	} else {
-		this.render('admin')
-	}
-})
+Router.route('/admin', {
+  name: 'admin',
+  onBeforeAction: function () {
+    if (Meteor.userId()) {
+      Meteor.subscribe('settings');
+     this.next();
+    } else {
+      this.render('admin')
+    }
+  },
+  data: function () {
+   return Settings.findOne({})
+ }
+});
