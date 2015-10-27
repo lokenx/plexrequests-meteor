52f03c680d25e6f6cf0ccec17ff2a74f2827bcb8
Mikhail Delport <mikhail@delport.ca>
2015-10-27 11:19:59 -0400

    Added tests for CouchPotato/SickRage/Sonarr/Pushbullet/Pushover

--- client/templates/admin/admin.js
+++ client/templates/admin/admin.js
@@ -34,5 +34,63 @@ Template.admin.events({
   'submit #updateSettingsForm' : function (event) {
     event.preventDefault();
     return false;
+  },
+  'click #couchPotatoTest' : function (event) {
+    event.preventDefault();
+    var btn = $(event.target);
+    btn.html("Testing... <i class='fa fa-spin fa-refresh'></i>").removeClass().addClass("btn btn-info-outline");
+    Meteor.call("testCouchPotato", function (error, result) {
+      if (error || !result) {
+        btn.removeClass("btn-info-outline").addClass("btn-danger-outline");
+        btn.html("Error!");
+      } else {
+        btn.removeClass("btn-info-outline").addClass("btn-success-outline");
+        btn.html("Success!");
+      }
+    })
+  },
+  'click #sickRageTest' : function (event) {
+    event.preventDefault();
+    var btn = $(event.target);
+    btn.html("Testing... <i class='fa fa-spin fa-refresh'></i>").removeClass().addClass("btn btn-info-outline");
+    Meteor.call("testSickRage", function (error, result) {
+      if (error || !result) {
+        btn.removeClass("btn-info-outline").addClass("btn-danger-outline");
+        btn.html("Error!");
+      } else {
+        btn.removeClass("btn-info-outline").addClass("btn-success-outline");
+        btn.html("Success!");
+      }
+    })
+  },
+  'click #pushbulletTest' : function (event) {
+    event.preventDefault();
+    var btn = $(event.target);
+    btn.html("Testing... <i class='fa fa-spin fa-refresh'></i>").removeClass().addClass("btn btn-info-outline");
+    Meteor.call("testPushbulllet", function (error, result) {
+      if (error || !result) {
+        btn.removeClass("btn-info-outline").addClass("btn-danger-outline");
+        btn.html("Error!");
+        Bert.alert(error.reason, "danger");
+      } else if (result) {
+        btn.removeClass("btn-info-outline").addClass("btn-success-outline");
+        btn.html("Success!");
+      }
+    })
+  },
+  'click #pushoverTest' : function (event) {
+    event.preventDefault();
+    var btn = $(event.target);
+    btn.html("Testing... <i class='fa fa-spin fa-refresh'></i>").removeClass().addClass("btn btn-info-outline");
+    Meteor.call("testPushover", function (error, result) {
+      if (error || !result) {
+        btn.removeClass("btn-info-outline").addClass("btn-danger-outline");
+        btn.html("Error!");
+        Bert.alert(error.reason, "danger");
+      } else if (result) {
+        btn.removeClass("btn-info-outline").addClass("btn-success-outline");
+        btn.html("Success!");
+      }
+    })
   }
 });
---
+++ server/methods/admin/servicesTests.js
@@ -0,0 +1,40 @@
+Meteor.methods({
+  testCouchPotato: function () {
+    return CouchPotato.appAvailable();
+  },
+  testSickRage: function () {
+    return SickRage.available();
+  },
+  testSonarr: function () {
+    return Sonarr.systemStatus();
+  },
+  testPushbulllet: function () {
+    var access_token = Settings.find().fetch()[0].pushbulletAPI;
+
+    try {
+      var test = HTTP.post("https://api.pushbullet.com/v2/pushes",
+        {headers: {'Access-Token': access_token},
+        params: {type: 'note', title: 'Plex Requests', body: 'Test notification!'},
+        timeout: 4000});
+
+      return true;
+    } catch (error) {
+      throw new Meteor.Error(401, error.response.data.error.message);
+    }
+  },
+  testPushover: function () {
+    var settings = Settings.find().fetch()[0];
+    var access_token = settings.pushoverAPI;
+    var user_key = settings.pushoverUSER;
+
+    try {
+      var test = HTTP.post("https://api.pushover.net/1/messages.json",
+        {params: {token: access_token, user: user_key, title: 'Plex Requests', message: 'Test notification'},
+        timeout: 4000});
+
+      return true;
+    } catch (error) {
+      throw new Meteor.Error(401, error.response.data.errors[0]);
+    }
+  }
+});
