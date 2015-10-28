400de0ca587ea49f50aab0b0c20400d462501ac7
Mikhail Delport <mikhail@delport.ca>
2015-10-28 12:08:00 -0400

    Get posters for movies/tv shows on boot

--- server/methods/requests/getPosters.js
+++ server/methods/requests/getPosters.js
@@ -1,13 +1,22 @@
 Meteor.methods({
   getPosters: function () {
-    var movies = Movies.find().fetch();
+    var movies = Movies.find({$or: [{poster_path: {$exists: false}}, {poster_path: "/"}]}).fetch();
+    var tv = TV.find({$or: [{poster_path: {$exists: false}}, {poster_path: "/"}]}).fetch();
 
     movies.forEach(function (movie) {
       var poster = Meteor.call("movie", movie.id);
 
       if (!poster) { poster = "/" }
-      
       Movies.update(movie._id, {$set: {
+        poster_path: poster
+      }});
+    });
+
+    tv.forEach(function (show) {
+      var poster = Meteor.call("tv", show.id);
+      console.log(poster);
+      if (!poster) { poster = "/" }
+      TV.update(tv._id, {$set: {
         poster_path: poster
       }});
     });
