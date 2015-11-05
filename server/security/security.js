960f6855b9ff5b01b6991bfc42a02bcf166e3fb3
Mikhail Delport <mikhail@delport.ca>
2015-11-05 17:00:51 -0500

    Proper bootstrap login form for admin, css fixes for settings list

--- server/security/security.js
+++ server/security/security.js
@@ -1,13 +1,5 @@
 Settings.permit('update').ifLoggedIn().apply();
 
-// Only allow a single admin
-if (Meteor.users.find({}).count() != 0) {
-  Accounts.config({
-    forbidClientAccountCreation : true
-  });
-}
-
-// Validate username, sending a specific error message on failure.
 Accounts.validateNewUser(function (user) {
   if (Meteor.users.find({}).count() === 0)
     return true;
