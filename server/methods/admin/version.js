cc508a08e6470aad167edb0b4f4b73ba0a5d3166
Mikhail Delport <mikhail@delport.ca>
2015-11-09 12:00:12 -0500

    Fixed admin links #167

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.1.4";
+    return "1.1.5";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
