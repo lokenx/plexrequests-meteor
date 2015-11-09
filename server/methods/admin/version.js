8297381119020cc8a9b70ff67e1960b2d1dec4a6
Mikhail Delport <mikhail@delport.ca>
2015-11-09 11:19:07 -0500

    URL fix for home link in nav when using a sub-directory

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.1.2";
+    return "1.1.3";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
