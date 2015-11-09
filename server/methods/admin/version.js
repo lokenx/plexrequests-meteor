6f5894a7bc16d1236a927b32eb7c9577a1c7e7f4
Mikhail Delport <mikhail@delport.ca>
2015-11-09 10:55:59 -0500

    Widened all views to 10 columns from 8

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.1.1";
+    return "1.1.2";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
