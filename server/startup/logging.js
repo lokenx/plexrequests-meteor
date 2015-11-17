ba720f6ca3878b9f2d691457e526088a5a0bc3db
loken <mikhail@delport.ca>
2015-11-17 10:40:46 -0500

    Fix for running in dev mode and Windows

--- server/startup/logging.js
+++ server/startup/logging.js
@@ -1,13 +1,23 @@
 Meteor.startup(function () {
   //creating a global server logger
   logger = Meteor.npmRequire('winston');
-  var appPath = process.env.PWD;
+  var FS = Npm.require('fs');
+  var Path = Npm.require('path');
+  var meteor_root = FS.realpathSync( process.cwd() + '/../' );
+  var application_root = FS.realpathSync( meteor_root + '/../' );
+
+  // if running on dev mode
+  if( Path.basename(FS.realpathSync( meteor_root + '/../../../' ) ) == '.meteor' ){
+      application_root =  FS.realpathSync( meteor_root + '/../../../../' );
+  }
 
   logger.add(logger.transports.File, {
-    filename: appPath + '/logs.log',
+    filename: Path.join(application_root + '\logs.log'),
     handleExceptions: true,
     humanReadableUnhandledException: true,
-    exitOnError: false
+    exitOnError: false,
+    maxsize: 100,
+    maxFiles: 3
   });
 
   logger.remove(logger.transports.Console);
