c9113d8a25fe8956f0cad53b162cdb5fa1d47073
Mikhail Delport <mikhail@delport.ca>
2015-11-16 20:38:06 -0500

    Version update for logging

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.2.0";
+    return "1.3.0";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
