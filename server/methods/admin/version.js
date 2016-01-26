c9f597c372a99abcdbdfe8892df0fb53122c8606
loken <mdelport@dyn129-100-123-141.has.uwo.ca>
2016-01-26 15:37:32 -0500

    Sooner wrapper version update

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.5.0";
+    return "1.5.1";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
