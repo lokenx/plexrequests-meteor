e66c9aa6ec0ea8380086fb73f3d04a6e5af56457
lokenx <mikhail@delport.ca>
2016-02-12 10:01:23 -0500

    Fixed hard coded login link
    Closes #257

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.5.3";
+    return "1.5.4";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
