13c533716b05bcaf553e97d6e76c5289a7a17cff
Leon Kunert <lkunert@cellular.de>
2016-02-15 10:07:48 +0100

    Updated Version.

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.5.4";
+    return "1.6.0";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
