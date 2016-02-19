87542f45ba67809d0ca3cdce02924a85fe2446ed
Leon Kunert <info@leonkunert.de>
2016-02-19 20:50:42 +0100

    Update version.js

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.6.0";
+    return "1.6.1";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
