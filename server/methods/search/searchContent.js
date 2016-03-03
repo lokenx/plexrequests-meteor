f3f1569238828a07eae31dd22f2b47e4243e11b9
Ricky Grassmuck <rigrassm@gmail.com>
2016-03-02 18:11:52 -0600

    commited to wrong branch Revert "Testing new TV data provider"
    This reverts commit 57c8be7380410f214e6af4b6ac218983fd381c80.

--- server/methods/search/searchContent.js
+++ server/methods/search/searchContent.js
@@ -6,37 +6,18 @@ Meteor.methods({
 
 		if (searchType !== "Music") {
 			var type = (searchType === "Movies") ? "movie" : "tv";
-			
-			    if (type === "tv") {
-		    		
-                    try {
-					    var result = Meteor.call("TVMAZESearch", searchterm, type)
-                    				
-			        } catch (error) {
-				        logger.error("TVMAZESearch Error -> " + error.message);
-				        return [];
-			        
-                    }
-			    
-                } else if (type === "movie") {
-		    		
-                    try {
-				        var result = Meteor.call("TMDBSearch", searchterm, type)
-                    				
-			        } catch (error) {
-				        logger.error("TMDBSearch Error -> " + error.message);
-				        return [];
-			        
-                    }
-			    }
-		
-            return result;
-		
-        } else {
+
+			try {
+				var result = Meteor.call("TMDBSearch", searchterm, type)
+			} catch (error) {
+				logger.error("TMDBSearch Error -> " + error.message);
+				return [];
+			}
+			return result;
+		} else {
 			return [];
 		}	
 	},
-
 	"searchOptions": function () {
 		var options = [];
 
