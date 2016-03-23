c215ad591564d250baaaf22da3002cb08e5a8d19
loken <mikhail@delport.ca>
2016-03-23 14:24:00 -0400

    Switched request images to use HTTPS

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.8.2";
+    return "1.8.3";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
