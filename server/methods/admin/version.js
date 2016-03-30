79dadae039dae669d1dd8ecd9ca9673d5298e169
Ricky Grassmuck <rigrassm@gmail.com>
2016-03-30 12:09:56 -0500

    Fixed TVMaze search result indexing issue

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.8.4";
+    return "1.8.5";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
