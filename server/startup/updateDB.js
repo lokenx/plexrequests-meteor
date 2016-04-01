41c1facdac76909a5ef308adbeec456d9411e1c6
Ricky Grassmuck <rigrassm@gmail.com>
2016-04-01 12:27:19 -0500

    Remove duplicate file and update version

--- server/startup/updateDB.js
+++
@@ -1,9 +0,0 @@
-Meteor.methods({
-	'updatePosters': function() {
-		var results = Movies.find({poster_path: /^\//}).fetch()[0];
-		if(results.poster_path){
-			Movies.update(results._id, { $set: {poster_path: "https://image.tmdb.org/t/p/w154"+results.poster_path}})
-		}
-	}
-});
-
