d32e59b2bc3dde91137d542377b097836e083c14
Ricky Grassmuck <rigrassm@gmail.com>
2016-04-01 19:18:48 +0000

    Poster_Path Fix
    * Update existing requests poster paths to be full URL's
    
    * Remove duplicate file and update version
    
    Close #311

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.8.5";
+    return "1.8.6";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
