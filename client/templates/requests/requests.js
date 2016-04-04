3adb9962b39be4d1f0ad65146a982a505fb7b881
Ricky Grassmuck <rigrassm@gmail.com>
2016-04-04 15:12:41 -0500

    Added https for TVMaze posters

--- client/templates/requests/requests.js
+++ client/templates/requests/requests.js
@@ -95,7 +95,8 @@ Template.requests.helpers({
 		if ((typeof this.poster_path === 'undefined') | (this.poster_path === "/")) {
 			return "poster-placeholder.png";
 		} else {
-                return this.poster_path;
+            var s_poster_path = this.poster_path.replace("http", "https");    
+			return s_poster_path;
 		}
 
   },
