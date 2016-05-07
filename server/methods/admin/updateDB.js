31da1529106b7b780a1f00faa5c9c52fd02c3c4e
loken <mikhail@delport.ca>
2016-05-07 13:17:38 -0400

    Error checking for updating seasons for TV shows

--- server/methods/admin/updateDB.js
+++ server/methods/admin/updateDB.js
@@ -1,12 +1,14 @@
 Meteor.methods({
-	'updatePosters': function() {
+	'updateSeasons': function() {
 		var results = TV.find({seasons: -1}).fetch();
-		for(i=0;i<results.length;i++){
-            var response = HTTP.call("GET", "http://api.tvmaze.com/shows/" + results[i].id + "/seasons", {})
-            var seasons = response.data; 
-           
-            TV.update({_id: results[i]._id}, { $set: {seasons: seasons.length}});
+		for(i = 0; i < results.length; i++) {
+			try {
+				var response = HTTP.call("GET", "http://api.tvmaze.com/shows/" + results[i].id + "/seasons", {});
+				var seasons = response.data;
+				TV.update({_id: results[i]._id}, { $set: {seasons: seasons.length}});
+			} catch (e) {
+				logger.warn("[Not Found]: Couldn't update seasons for", results[i].title);
+			}
 		}
 	}
 });
-
--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.11.0";
+    return "1.11.1";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
