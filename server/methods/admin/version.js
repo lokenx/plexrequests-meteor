5be41f39d7c35eaa542943c63db5a084db230720
Ricky Grassmuck <rigrassm@gmail.com>
2016-05-13 18:30:59 -0500

    Added Systemd unit file template located under include/systemd (#348)
    * Added Systemd unit file template located under include/systemd
    
    * Changed Unit Description
    
    Changed unit description to provide a more accurate reflection of what the service file does.
    
    * Version Bump

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.12.0";
+    return "1.12.1";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
