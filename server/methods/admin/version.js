1d457a147d675d2e99f04e9de260395c89afef3a
loken <mikhail@delport.ca>
2016-05-23 12:30:51 -0400

    Version increase of user changes PR #352

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -3,7 +3,7 @@ Meteor.methods({
     return "master";
   },
   getVersion: function () {
-    return "1.13.0";
+    return "1.14.0";
   },
   checkForUpdate : function () {
     var branch = Meteor.call('getBranch');
