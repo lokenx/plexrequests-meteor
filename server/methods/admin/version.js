f372e7dc01b9f9556eec67a520120f3a5a685b90
Ricky Grassmuck <rigrassm@gmail.com>
2017-06-28 01:30:19 -0500

    Version/Update Check and Admin Page Style Fixes - Refactor version checking method to pull from version.txt and the latest release tag on git - Fix Admin page sidebar styling issue where labels would go outside the sidebars background - Version Bump v1.20.7

--- server/methods/admin/version.js
+++ server/methods/admin/version.js
@@ -4,31 +4,24 @@ Meteor.methods({
     },
 
     getVersion: function () {
-        return "1.20.6";
+        // parse the version.txt file and return the contents
+        var fs = Npm.require('fs');
+        return 'v' + fs.readFileSync(process.env.PWD + '/version.txt','utf8');
     },
 
     checkForUpdate : function () {
-        var branch = Meteor.call('getBranch');
         var currentVersion = Meteor.call('getVersion');
+        // Only the master branch is versioned and has tagged releases
+        if (Meteor.call('getBranch') === "master") {
+            try {
+                var latestJson = HTTP.call("GET", "https://api.github.com/repos/lokenx/plexrequests-meteor/releases/latest", {headers: {"User-Agent": "Plexrequests/" + currentVersion}});
+            }
+            catch (err) {
+                logger.info("Error checking for update: " + err);
+                return false;
+            }
 
-        try {
-            var latestJson = HTTP.call("GET","https://api.github.com/repos/lokenx/plexrequests-meteor/contents/version.txt?ref=" + branch,{headers: {"User-Agent": "Meteor/1.1"}});
-        }
-        catch (err) {
-            logger.log("Error checking for update: " + err);
-            return false;
-        }
-
-        var latestJson64 = latestJson['data']['content'];
-        var latestVersion64 = new Buffer(latestJson64, "base64").toString();
-        var latestVersion = latestVersion64.slice(0, - 1);
-
-        if (latestVersion != currentVersion) {
-            logger.info("New update available");
-            return true;
-        } else {
-            return false
+            return (latestJson.data.tag_name !== currentVersion);
         }
     }
-
 });
