69a3e6dc5591f6ca249aa9a256e151b3359c87c6
Jagandeep Brar <me@jagandeepbrar.io>
2017-10-25 15:33:22 -0500

    Add Pushbullet Device ID Support

--- server/methods/notifications/pushbulletNotification.js
+++ server/methods/notifications/pushbulletNotification.js
@@ -8,47 +8,41 @@ Meteor.methods({
 
         var access_token = settings.pushbulletAPI
         var channel_tag = settings.pushbulletChannel
+        var device_id = settings.pushbulletDeviceID
         var pushbullet_url = 'https://api.pushbullet.com/v2/pushes'
-        var payload = {}
 
-        //////// Create JSON object of http.post Parameters
-        if(typeof channel_tag === 'undefined' ) {
-            payload = {
-                headers: {
-                    'Access-Token': access_token
-                },
-                params: {
-                    type: 'note',
-                    title: title,
-                    body: body
-                },
-                timeout: 4000
-            }
-            logger.debug('Empty Channel')
-        } else {
-            payload = {
-                headers: {
-                    'Access-Token': access_token
-                },
-                params: {
-                    type: 'note',
-                    title: title,
-                    body: body,
-                    channel_tag: channel_tag
-                },
-                timeout: 4000
-            }
-            logger.debug('channel_tag: ' + channel_tag)
+        //Return with error if both channel and device are defined
+        if(typeof channel_tag !== 'undefined' && typeof device_id !== 'undefined') {
+            var err = 'Please only use either a channel or a device ID in Pushbullet!'
+            logger.error('Pushbullet notification error: ' + err)
+            throw err
+        }
+        
+        //Build base payload
+        var payload = {
+            headers: {
+                'Access-Token': access_token
+            },
+            params: {
+                type: 'note',
+                title: title,
+                body: body
+            },
+            timeout: 4000
         }
 
-        ////// Attempt to send notification with HTTP.post()
+        //Add channel or device id parameters to the payload if applicable
+        if(typeof channel_tag !== 'undefined' ) {
+            payload['params'].channel_tag = channel_tag
+        } else if(typeof device_id !== 'undefined') {
+            payload['params'].device_iden = device_id
+        }
 
+        //Attempt to send notification with HTTP.post()
         try {
             HTTP.post(pushbullet_url, payload)
             return true
-        }
-	
-        catch (error) {
+        } catch (error) {
             var err = error.response.data.error.message
             logger.error('Pushbullet notification error: ' + err)
             throw err
